632f86de3bf9467b94c2b7edec307721e09767c8
Unknown <elhadji.diallo@edu.itescia.fr>
2019-06-19 14:24:46 +0200

    Header and Footer page done

---
+++ src/Head.php
@@ -0,0 +1,10 @@
+<header>
+  <center> <a href="homapage.php"><img src="Smart_round.png" style="height:100px"></a></center>
+  <div class="bar">
+    <a href="Admin.php" target="_blank"><button class="but" type="button" name="button" onclick="Admin.php">Administrateur</button></a>
+    <a href="Gestionnaire.php" target="_blank"><button class="but" type="button" name="button" >Exercice</button></a>
+    <a href="Technicien.php" target="_blank"><button class="but" type="button" name="button">La Poste</button></a>
+    <a href="Véhicule.php" target="_blank"><button class="but" type="button" name="button" >Exercice</button></a>
+    <a href="Mainteance.php" target="_blank"><button class="but" type="button" name="button">La Poste</button></a>
+  </div>
+</header>
