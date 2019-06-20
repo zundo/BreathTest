92c16935e0e793e8fbfed26f25472220b544562f
Unknown <elhadji.diallo@edu.itescia.fr>
2019-06-20 15:16:20 +0200

    HomePage and Style

---
+++ homepage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <?php require "src/Head.php"; ?>
+    <center>
+      <p > Move Smart & Improve Dynamically</p>
+    </center>
+    <?php require "src/footer.php"; ?>
+  </body>
+</html>
